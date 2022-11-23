eba8ac32026cd7b287caeecde22643b992f1a1bb
Anastasiya Sakharuk <sakharuk333@gmail.com>
2022-11-23 21:17:39 +0300

    Max method

--- src/main/java/com/epam/rd/autotasks/max/MaxMethod.java
+++ src/main/java/com/epam/rd/autotasks/max/MaxMethod.java
@@ -1,7 +1,17 @@
 package com.epam.rd.autotasks.max;
 
+import java.util.Arrays;
+
 public class MaxMethod {
+
     public static int max(int[] values) {
-        throw new UnsupportedOperationException();
+        Arrays.sort(values);
+        System.out.println("intArray after sorting: " + Arrays.toString(values));
+        return values[values.length - 1];
+    }
+
+    public static void main(String[] args) {
+        int[] intArray = {-24, 340, 0, 34, 12, 10, 20};
+        System.out.println("Maximum number = " + max(intArray));
     }
 }
